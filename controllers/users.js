73b9cb50888352cd021ea4e85991a7b1f0abfffc
Ryabtseva-Ekaterina <ekaterina-ryabtseva.evp@yandex.ru>
2022-08-05 07:06:54 +0300

    test errors: post

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,3 @@
-const {incorrectDataError, notFoundError} = require ('../error')
-
 const User = require('../models/user.js');
 
 module.exports.getUsers = (req, res) => {
@@ -31,10 +29,12 @@ module.exports.getUser = (req, res) => {
 }
 
 module.exports.createUser= (req, res) => {
-  const { name, about } = req.body;
-  User.create({ name, about })
-    .then(user => res.send({ data: user }))
+  console.log(req.body);
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then(user => res.status(200||201).send({ user }))
     .catch((err) => {
+      console.log(err);
       if (err.name === 'incorrectDataError') {
         res.status (400). send( {
           "message" : "Переданы некорректные данные при создании пользователя. "
